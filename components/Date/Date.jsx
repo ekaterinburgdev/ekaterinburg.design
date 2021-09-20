32bba826c63bed0cf7d3a68e4b6029f2944bc009
Alexander Chabin <nibach@ya.ru>
2021-09-20 18:21:43 +0500

    Установил русский языык в контроле Date

--- components/Date/Date.jsx
+++ components/Date/Date.jsx
@@ -1,6 +1,7 @@
 import { parseISO, format } from 'date-fns'
+import { ru } from 'date-fns/locale'
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy', { locale: ru })}</time>
 }
