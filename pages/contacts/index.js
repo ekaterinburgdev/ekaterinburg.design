030faa81fd7ba200bfe425d2fb9284ca30dbb817
Alexander Chabin <nibach@ya.ru>
2021-09-22 01:42:17 +0500

    Вывел данные о контактах

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,14 +1,35 @@
 import Layout from '../../components/Layout'
 import Head from 'next/head'
 
+
+const contacts = [
+  {
+    caption: 'mail@ekaterinburg.design',
+    link: 'mailto:mail@ekaterinburg.design'
+  },
+  {
+    caption: 'группа в фейсбуке',
+    link: 'https://www.facebook.com/groups/ekaterinburg.design/'
+  },
+  {
+    caption: 'инстаграм',
+    link: 'https://www.instagram.com/ekaterinburg.design/'
+  }
+]
+
 export default function Contacts() {
   return (
     <Layout>
       <Head>
-        <title>Контакты</title>
+        <title>Почта и соцсети</title>
       </Head>
-      Тест
-    </Layout>
+
+      <ul>
+        {contacts.map(({ caption, link }) =>
+          <li><a href={link} target="_blank">{caption}</a></li>
+        )}
+      </ul>
+    </Layout >
   )
 }
 
