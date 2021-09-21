8fdcfbded3cfea713f828acf85536a4fd90d4192
Alexander Chabin <nibach@ya.ru>
2021-09-22 01:49:18 +0500

    Добавил ссылки на участников проекта

--- pages/team/index.js
+++ pages/team/index.js
@@ -3,16 +3,19 @@ import Head from 'next/head'
 
 const team = [
   {
+    "link": "https://www.facebook.com/albydesign",
     "name": "Алексей Быков",
     "image": "https://ekaterinburg.design/data/team/lesha-userpic@2x.jpg",
     "role": "промышленный дизайнер"
   },
   {
+    "link": "https://www.facebook.com/profile.php?id=100000719476103",
     "name": "Владислав Деревянных",
     "image": "https://ekaterinburg.design/data/team/vlad-userpic@2x.jpg",
     "role": "дизайнер"
   },
   {
+    "link": "https://www.facebook.com/profile.php?id=100008241100470",
     "name": "Елена Габдлахатова",
     "image": "https://ekaterinburg.design/data/team/lena-userpic@2x.jpg",
     "role": "дизайнер"
@@ -23,31 +26,37 @@ const team = [
     "role": "директор информационно-туристической службы"
   },
   {
+    "link": "https://omelekhin.ru/",
     "name": "Паша Омелёхин",
     "image": "https://ekaterinburg.design/data/team/pasha-userpic@2x.jpg",
     "role": "дизайнер"
   },
   {
+    "link": "https://www.facebook.com/dimgafat",
     "name": "Дмитрий Фатхиев-Долохов",
     "image": "https://ekaterinburg.design/data/team/dima-userpic@2x.jpg",
     "role": "иллюстратор"
   },
   {
+    "link": "https://www.facebook.com/profile.php?id=100002169084168",
     "name": "Дмитрий Фогель",
     "image": "https://ekaterinburg.design/data/team/dmitry-userpic@2x.jpg",
     "role": "главный художник города"
   },
   {
+    "link": "https://www.facebook.com/mchereda",
     "name": "Михаил Череда",
     "image": "https://ekaterinburg.design/data/team/misha-userpic@2x.jpg",
     "role": "дизайнер"
   },
   {
+    "link": "https://www.facebook.com/sergey.shashmurin.1",
     "name": "Сергей Шашмурин",
     "image": "https://ekaterinburg.design/data/team/serezha-userpic@2x.jpg",
     "role": "промышленный дизайнер"
   },
   {
+    "link": "https://www.facebook.com/harrisov",
     "name": "Никита Харисов",
     "image": "https://ekaterinburg.design/data/team/nikita-userpic@2x.jpg",
     "role": "креативный директор «Атомстройкомплекса»"
@@ -60,11 +69,13 @@ export default function Team() {
       <Head>
         <title>Команда</title>
       </Head>
-      {team.map(({ name, image, role }) => (
-        <figure key={name}>
-          <img src={image} alt={name} width={100} height={100} />
-          <figcaption>{name}, {role}</figcaption>
-        </figure>
+      {team.map(({ link, name, image, role }) => (
+        <a href={link} target="_blank">
+          <figure key={name}>
+            <img src={image} alt={name} width={100} height={100} />
+            <figcaption>{name}, {role}</figcaption>
+          </figure>
+        </a>
       ))}
     </Layout>
   )
