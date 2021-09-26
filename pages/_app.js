d91f6b42fc1591ebbe098ac57c02c38a0cd32b30
Alexander Chabin <nibach@ya.ru>
2021-09-27 02:27:56 +0500

    Подключил полоску-loader при переходах между страницами

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+import 'nprogress/nprogress.css'
 import '../styles/global.scss'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+NProgress.configure({ showSpinner: false })
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done()) 
+
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
 }
+
+export default MyApp
