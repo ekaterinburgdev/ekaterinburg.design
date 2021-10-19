3398ba9093ec633b61e7f4b5031d02cd84341000
Alexander Chabin <nibach@ya.ru>
2021-10-19 17:56:41 +0500

    Обновил типографику

--- pages/index.js
+++ pages/index.js
@@ -14,19 +14,20 @@ export default function Home({ projects }) {
       </Head>
 
       <section>
-        <p>
-          «Дизайн-код Екатеринбурга» — инициативный проект независимых дизайнеров города. Мы разрабатываем единые визуальные стандарты городской среды и внедряем их в жизнь.
-          <br />
-          Наша цель — сделать Екатеринбург уютным и комфортным, чтобы жителям хотелось остаться, а гостям захотелось вернуться.
+        <p className={'site-description site-description_align-left'}>
+          &laquo;Дизайн-код Екатеринбурга&raquo;&nbsp;&mdash; инициативный проект независимых дизайнеров города. Мы&nbsp;разрабатыва&shy;ем единые визуальные стандарты городской среды и&nbsp;внедряем их&nbsp;в&nbsp;жизнь.
+        </p>
+        <p className={'site-description site-description_align-right'}>
+          Наша цель&nbsp;&mdash; сделать Екатеринбург уютным и&nbsp;комфортным, чтобы жите&shy;лям хотелось остаться, а&nbsp;гостям захотелось вернуться.
         </p>
 
-        <h2><Link href={`/partners`}>Партнеры</Link></h2>
-        <h2><Link href={`/team`}>Команда</Link></h2>
-        <h2><Link href={`/contacts`}>Почта и соцсети</Link></h2>
+        <h2 className={'section-heading'}><Link href={`/partners`}>Партнеры</Link></h2>
+        <h2 className={'section-heading'}><Link href={`/team`}>Команда</Link></h2>
+        <h2 className={'section-heading'}><Link href={`/contacts`}>Почта и соцсети</Link></h2>
 
 
         <section>
-          <h2>Проекты</h2>
+          <h2 className={'section-heading'}>Проекты</h2>
           <PostPreviewGrid posts={projects} />
         </section>
       </section>
