d3dda884e68a6061b8b0263854a01e2d787a7683
Alexander Chabin <nibach@ya.ru>
2021-10-19 07:28:42 +0500

    Удалил неиспользуемый компонент Date

--- components/Date/Date.jsx
+++
@@ -1,7 +0,0 @@
-import { parseISO, format } from 'date-fns'
-import { ru } from 'date-fns/locale'
-
-export default function Date({ dateString }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy', { locale: ru })}</time>
-}
