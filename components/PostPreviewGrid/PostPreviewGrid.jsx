d54c14212b0ef088c32a9eb23eea0f45842f80e5
Alexander Chabin <nibach@ya.ru>
2021-11-09 04:42:41 +0500

    Добавил key для элементов списка с превью постов

--- components/PostPreviewGrid/PostPreviewGrid.jsx
+++ components/PostPreviewGrid/PostPreviewGrid.jsx
@@ -11,7 +11,7 @@ export default function PostPreviewGrid({ posts }) {
   return (
     <ul className={cx('post-preview-grid')}>
       {posts.map(({ highlight, title, preview: gallery }) => (
-        <li className={cx('post-preview-grid__item', { 'post-preview-grid__item_highlight': highlight })}>
+        <li className={cx('post-preview-grid__item', { 'post-preview-grid__item_highlight': highlight })} key={title}>
           <PostPreview title={title} gallery={gallery} highlight={highlight} />
         </li>
       ))}
