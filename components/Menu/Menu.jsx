46b2dd618ec33fdb24af6b7ec73015d35741f305
Alexander Chabin <nibach@ya.ru>
2021-11-10 06:31:34 +0500

    Подсветил активный пункт меню

--- components/Menu/Menu.jsx
+++ components/Menu/Menu.jsx
@@ -1,10 +1,39 @@
 import classNames from 'classnames/bind';
+import { element } from 'prop-types';
+import { useEffect } from 'react';
 
 import styles from './Menu.module.scss';
 
 const cx = classNames.bind(styles);
 
 export default function Menu({ items }) {
+  useEffect(() => {
+    // TODO сделать через useRef
+    const linkWithElementList = [...document.querySelectorAll(`.${cx('menu__item')}`)].reverse().map(link => {
+      const element = document.querySelector(link.getAttribute('href'));
+      return { link, element };
+    });
+    // TODO удалять обработчик, добавить троттлинг
+    window.addEventListener('scroll', () => {
+      let currentLink = null;
+
+      for (const { link, element } of linkWithElementList) {
+        if (isElementInViewport(element)) {
+          link.classList.add(cx('menu__item_active'));
+          currentLink = link;
+          break;
+        }
+      }
+
+      for (const { link } of linkWithElementList) {
+        if (currentLink !== link) {
+          link.classList.remove(cx('menu__item_active'));
+        }
+      }
+    });
+
+  }, []);
+
   return <>
     {items && (
       <ul className={cx('menu')}>
@@ -14,4 +43,15 @@ export default function Menu({ items }) {
       </ul>
     )}
   </>
+};
+
+function isElementInViewport(el, percentVisible = 10) {
+  let
+    rect = el.getBoundingClientRect(),
+    windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+
+  return !(
+    Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-rect.height) * 100)) < percentVisible ||
+    Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percentVisible
+  )
 };
