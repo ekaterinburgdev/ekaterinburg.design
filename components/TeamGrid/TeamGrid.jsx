64be4c940830a51fc64eafc45c3a88e481910f95
Alexander Chabin <nibach@ya.ru>
2022-01-08 20:04:38 +0500

    Добавил сортировку членов команды

--- components/TeamGrid/TeamGrid.jsx
+++ components/TeamGrid/TeamGrid.jsx
@@ -7,23 +7,37 @@ import styles from './TeamGrid.module.scss';
 const cx = classNames.bind(styles);
 
 export default function TeamGrid({ team }) {
+  const sortBySurname = (a, b) => {
+    const getSurname = (item) => item['имя'].split(' ').pop();
+    const surnameA = getSurname(a);
+    const surnameB = getSurname(b);
+
+    if (surnameA < surnameB) { return -1; }
+    if (surnameA > surnameB) { return 1; }
+    return 0;
+  }
+
   return (
     team && <ul className={cx("team-grid")}>
-      {team?.filter(({ ['должность на сайте'] : role }) => role !== 'undefined').map(({
-        ['имя']: name,
-        ['фото']: photo,
-        ['ссылка на сайте']: link,
-        ['должность на сайте']: role
-      }) => (
-        <li className={cx("team-grid__item")} key={name}>
-          <TeamPerson
-            name={name}
-            role={role}
-            photo={photo}
-            link={link}
-          />
-        </li>
-      ))}
+      {team
+        ?.filter(({ ['должность на сайте']: role }) => role !== 'undefined')
+        .sort(sortBySurname)
+        .map(({
+          ['имя']: name,
+          ['фото']: photo,
+          ['ссылка на сайте']: link,
+          ['должность на сайте']: role
+        }) => (
+          <li className={cx("team-grid__item")} key={name}>
+            <TeamPerson
+              name={name}
+              role={role}
+              photo={photo}
+              link={link}
+            />
+          </li>
+        ))}
     </ul>
   )
 };
+
