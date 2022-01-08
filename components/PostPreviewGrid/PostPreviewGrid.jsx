1505ea533eb7d32c5e64d54df250fb8053166a22
Alexander Chabin <nibach@ya.ru>
2022-01-08 20:42:38 +0500

    Добавил сортировку проектов по приоритету

--- components/PostPreviewGrid/PostPreviewGrid.jsx
+++ components/PostPreviewGrid/PostPreviewGrid.jsx
@@ -8,13 +8,22 @@ import styles from './PostPreviewGrid.module.scss'
 const cx = classNames.bind(styles);
 
 export default function PostPreviewGrid({ posts }) {
+  const sortByPriority = (a, b) => {
+    if (a.priority < b.priority) { return -1; }
+    if (a.priority > b.priority) { return 1; }
+    return 0;
+  }
+
   return (
     <ul className={cx('post-preview-grid')}>
-      {posts.map(({ id, highlight, title, preview: gallery }) => (
-        <li className={cx('post-preview-grid__item', { 'post-preview-grid__item_highlight': highlight })} key={title}>
-          <PostPreview id={id} title={title} gallery={gallery} highlight={highlight} />
-        </li>
-      ))}
+      {posts
+        .filter(({ published }) => published)
+        .sort(sortByPriority)
+        .map(({ id, highlight, title, preview: gallery }) => (
+          <li className={cx('post-preview-grid__item', { 'post-preview-grid__item_highlight': highlight })} key={title}>
+            <PostPreview id={id} title={title} gallery={gallery} highlight={highlight} />
+          </li>
+        ))}
     </ul>
   )
 };
