ee68cf48ce7e1591ac07f9ac06731391f103749c
Alexander Chabin <nibach@ya.ru>
2022-01-09 02:12:06 +0500

    Расширил надпись с копирайтом

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -9,29 +9,31 @@ import styles from './Layout.module.scss';
 
 export default function Layout({ children, home, menuItems }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
+    <>
+      <div className={styles.container}>
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
 
-      <Header menuItems={menuItems} />
+        <Header menuItems={menuItems} />
 
-      <main>
-        {children}
-      </main>
+        <main>
+          {children}
+        </main>
 
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← На главную</a>
-          </Link>
-        </div>
-      )}
+        {!home && (
+          <div className={styles.backToHome}>
+            <Link href="/">
+              <a>← На главную</a>
+            </Link>
+          </div>
+        )}
+      </div>
 
       <Footer />
-    </div>
+    </>
   )
 }
