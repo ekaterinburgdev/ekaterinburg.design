1702e8066b2e7411ea495a6263923c8488c5b414
Alexander Chabin <nibach@ya.ru>
2022-01-08 19:23:08 +0500

    Добавил копирайт с длинным тире

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -7,7 +7,12 @@ const cx = classNames.bind(styles);
 export default function Footer() {
   return (
     <footer className={cx('footer')}>
-      Дизайн-код Екатеринбурга · 2017–2021
+      Дизайн-код Екатеринбурга
+
+      <div className={cx('footer__year')}>
+        2017
+        <div className={cx('footer__year-dash')}></div>
+      </div>
     </footer>
   )
 };
