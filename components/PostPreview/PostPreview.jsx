7d5a3e6d9e0e4edc625977191ba4fbc18bd2d185
Alexander Chabin <nibach@ya.ru>
2022-01-08 13:23:31 +0500

    Показываю название проекта по ховеру

--- components/PostPreview/PostPreview.jsx
+++ components/PostPreview/PostPreview.jsx
@@ -10,6 +10,13 @@ const cx = classNames.bind(styles);
 export default function PostPreview({ id, title, gallery, highlight }) {
   return (
     <article className={cx('post-preview', { 'post-preview_highlight': highlight })}>
+      <a href={id} className={cx('post-preview__title')}>
+        <span className={cx('post-preview__title-caption')}>
+          {title}
+          <span className={cx('post-preview__title-underline')}>{title}</span>
+        </span>
+      </a>
+
       {gallery?.map(image => (
         <figure className={cx('post-preview__image')} key={image}>
           <img src={image} alt="" />
