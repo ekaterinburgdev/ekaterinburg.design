ceb28aa64112c782a0986e80d4c832123291ea55
Alexander Chabin <nibach@ya.ru>
2022-01-10 15:20:15 +0500

    Темный navbar на телефоне

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -11,6 +11,7 @@ export default function Layout({ children, home }) {
     <>
       <div className={styles.container}>
         <Head>
+          <meta name="theme-color" content="#000000" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
