bb98308867b9eb00de168bb7e0c5e1cb7db69bd7
Alexander Chabin <nibach@ya.ru>
2022-01-10 07:37:20 +0500

    Добавил блок "Кто мы"

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,30 @@ export default function Home({ projects, team, partners }) {
 
         <section id="about" className={'section'}>
           <Logo />
+
+          <div className={'main-visual'}>
+            <h2 className={'main-visual__title'}>
+              Мы — департамент<br />
+              дизайна<br />
+              Екатеринбурга
+            </h2>
+            <p className={'main-visual__description'}>
+              Мы отвечаем за всю красоту, комфорт и уют и создаём самый приятный город в России.
+
+              <svg className={'main-visual__logo'} width="207" height="170" viewBox="0 0 207 170" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clipPath="url(#clip0_1696_932)">
+                <path d="M206.003 99.79C137.335 77.6443 68.6677 138.149 0 116.003V161.953C68.6677 184.099 137.335 123.594 206.003 145.74V99.79Z" fill="#00B4FF"/>
+                <path d="M206.003 7.95005C137.335 -14.1957 68.6677 46.3086 0 24.1628V70.0828C68.6677 92.2286 137.335 31.7243 206.003 53.8701V7.95005Z" fill="#FFD400"/>
+                <path d="M206.003 43.8701C137.335 21.7244 68.6677 82.2286 0 60.0829V126.003C68.6677 148.149 137.335 87.6444 206.003 109.79V43.8701Z" fill="#00B400"/>
+                </g>
+                <defs>
+                <clipPath id="clip0_1696_932">
+                  <rect width="206.64" height="153.66" fill="white" transform="matrix(0.996917 -0.0784591 0 1 0 16.2129)"/>
+                </clipPath>
+                </defs>
+              </svg>
+            </p>
+          </div>
         </section>
 
         <section className={'section'} id="projects">
