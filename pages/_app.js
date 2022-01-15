7733200d06cb3843dd763df699a20e2ba8c82c68
Alexander Chabin <nibach@ya.ru>
2022-01-16 01:08:59 +0500

    Отключил белую полосу загрузки на главной

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import Router from 'next/router'
 import NProgress from 'nprogress'
 
-import 'nprogress/nprogress.css'
 import '../styles/global.scss'
+import 'nprogress/nprogress.css'
 
 NProgress.configure({ showSpinner: false })
-Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeStart', (url) => {
+    if (url !== '/') NProgress.start()
+})
+
 Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
