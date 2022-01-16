53c09f9c9fd2e13131b477c144e9f971bec12193
Alexander Chabin <nibach@ya.ru>
2022-01-16 19:35:23 +0500

    Обновил верстку блока с партнерами

--- components/Partners/Partners.jsx
+++ components/Partners/Partners.jsx
@@ -20,15 +20,15 @@ export default function Partners({ partners }) {
         )}
       </div>
 
-      <p className={cx('partners__description')}>
+      <p className={cx('partners__description', 'partners__description_left')}>
         Мы&nbsp;создаём дизайн-систему Екатеринбурга, которая исключит появление плохого дизайна в&nbsp;городе. Это сделает городскую среду приятнее и&nbsp;комфортнее.
       </p>
 
-      <p className={cx('partners__description')}>
+      <p className={cx('partners__description', 'partners__description_right')}>
         Чтобы делать это быстрее и&nbsp;эффективнее, мы&nbsp;ищем инвесторов и&nbsp;спонсоров, которые готовы поддержать развитие дизайна Екатеринбурга.
       </p>
 
-      <p className={cx('partners__description')}>
+      <p className={cx('partners__description', 'partners__description_left')}>
         Обсудить условия и&nbsp;обязанности с&nbsp;нашей стороны с&nbsp;вами готов наш руководитель Паша Омелёхин&nbsp;&mdash; <a href="mailto:pasha@ekateringburg.design">pasha@ekateringburg.design</a>
       </p>
     </div>
