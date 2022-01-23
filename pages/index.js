af65a585c551be72c9a259e838359a92ec25193e
Alexander Chabin <nibach@ya.ru>
2022-01-23 22:27:47 +0500

    Поправил адаптивность компонентов главной

--- pages/index.js
+++ pages/index.js
@@ -36,14 +36,14 @@ export default function Home({ projects, team, partners }) {
 
               <svg className={'main-visual__logo'} width="207" height="170" viewBox="0 0 207 170" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g clipPath="url(#clip0_1696_932)">
-                <path d="M206.003 99.79C137.335 77.6443 68.6677 138.149 0 116.003V161.953C68.6677 184.099 137.335 123.594 206.003 145.74V99.79Z" fill="#00B4FF"/>
-                <path d="M206.003 7.95005C137.335 -14.1957 68.6677 46.3086 0 24.1628V70.0828C68.6677 92.2286 137.335 31.7243 206.003 53.8701V7.95005Z" fill="#FFD400"/>
-                <path d="M206.003 43.8701C137.335 21.7244 68.6677 82.2286 0 60.0829V126.003C68.6677 148.149 137.335 87.6444 206.003 109.79V43.8701Z" fill="#00B400"/>
+                  <path d="M206.003 99.79C137.335 77.6443 68.6677 138.149 0 116.003V161.953C68.6677 184.099 137.335 123.594 206.003 145.74V99.79Z" fill="#00B4FF" />
+                  <path d="M206.003 7.95005C137.335 -14.1957 68.6677 46.3086 0 24.1628V70.0828C68.6677 92.2286 137.335 31.7243 206.003 53.8701V7.95005Z" fill="#FFD400" />
+                  <path d="M206.003 43.8701C137.335 21.7244 68.6677 82.2286 0 60.0829V126.003C68.6677 148.149 137.335 87.6444 206.003 109.79V43.8701Z" fill="#00B400" />
                 </g>
                 <defs>
-                <clipPath id="clip0_1696_932">
-                  <rect width="206.64" height="153.66" fill="white" transform="matrix(0.996917 -0.0784591 0 1 0 16.2129)"/>
-                </clipPath>
+                  <clipPath id="clip0_1696_932">
+                    <rect width="206.64" height="153.66" fill="white" transform="matrix(0.996917 -0.0784591 0 1 0 16.2129)" />
+                  </clipPath>
                 </defs>
               </svg>
             </p>
@@ -51,8 +51,15 @@ export default function Home({ projects, team, partners }) {
         </section>
 
         <section className={'section'} id="projects">
-          <h2 className={'section-heading section-heading_projects emerge'} data-expose="true">Дизайн-<br />система</h2>
-					<p className={'section-heading_caption emerge'} data-expose="true">Мы задаём стандарты дизайна Екатеринбурга и разрабатываем городские решения на собственном производстве. Умеем вести комплексные проекты: от разработки архитектурных форм до развития территорий. Мы уникальны, кроме нас так никто не может.</p>
+          <div className={'section-projects'}>
+            <h2 className={'section-heading section-heading_projects emerge'} data-expose="true">Дизайн-<br />система</h2>
+            <p className={'section-heading_caption emerge'} data-expose="true">
+              Мы задаём стандарты дизайна Екатеринбурга и разрабатываем городские решения на собственном производстве. Умеем вести комплексные проекты: от разработки архитектурных форм до развития территорий.
+              <br />
+              <br />
+              Мы уникальны, кроме нас так никто не может.
+            </p>
+          </div>
           <PostPreviewGrid posts={projects} />
         </section>
 
