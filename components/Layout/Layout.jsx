8fc7a03f518494f97ad7b2e8da15d2ce15775031
Alexander Chabin <nibach@ya.ru>
2022-02-04 23:26:38 +0500

    Запретил scale сайта на телефоне

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -21,6 +21,7 @@ export default function Layout({ children, home }) {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
           <meta property="og:type" content="website" />
           <meta property="og:url" content={`https://${process.env.VERCEL_URL}/`} />
