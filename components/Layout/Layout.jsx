1d23f3139c2f714fff50753672182e3cc6cf253e
Alexander Chabin <nibach@ya.ru>
2022-02-05 09:45:15 +0500

    Добавил язык страницы  (a11y)

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -8,34 +8,9 @@ import styles from './Layout.module.scss';
 
 
 export default function Layout({ children, home }) {
-  const siteTitle = 'Дизайн-код Екатеринбурга';
-  const siteDescription = '«Дизайн-код Екатеринбурга» — инициативный проект независимых дизайнеров города. Мы разрабатываем единые визуальные стандарты городской среды и внедряем их в жизнь. Наша цель — сделать Екатеринбург уютным и комфортным, чтобы жителям хотелось остаться, а гостям захотелось вернуться.';
-
   return (
     <>
       <div className={styles.container}>
-        <Head>
-          <meta name="theme-color" content="#000000" />
-          <meta name="description" content={siteDescription} />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content={`https://${process.env.VERCEL_URL}/`} />
-          <meta property="og:title" content={siteTitle} />
-          <meta property="og:description" content={siteDescription} />
-          <meta property="og:image" content="/og-preview.jpg" />
-
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content={`https://${process.env.VERCEL_URL}/`} />
-          <meta property="twitter:title" content={siteTitle} />
-          <meta property="twitter:description" content={siteDescription} />
-          <meta property="twitter:image" content="/og-preview.jpg"></meta>
-        </Head>
-
         <main>
           {children}
         </main>
@@ -50,7 +25,6 @@ export default function Layout({ children, home }) {
       </div>
 
       <Footer />
-
     </>
   )
 }
---
+++ pages/_document.js
@@ -0,0 +1,36 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  const siteTitle = 'Дизайн-код Екатеринбурга';
+  const siteDescription = '«Дизайн-код Екатеринбурга» — инициативный проект независимых дизайнеров города. Мы разрабатываем единые визуальные стандарты городской среды и внедряем их в жизнь. Наша цель — сделать Екатеринбург уютным и комфортным, чтобы жителям хотелось остаться, а гостям захотелось вернуться.';
+
+  return (
+    <Html lang="ru">
+      <Head>
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content={siteDescription} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={`https://${process.env.VERCEL_URL}/`} />
+        <meta property="og:title" content={siteTitle} />
+        <meta property="og:description" content={siteDescription} />
+        <meta property="og:image" content="/og-preview.jpg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={`https://${process.env.VERCEL_URL}/`} />
+        <meta property="twitter:title" content={siteTitle} />
+        <meta property="twitter:description" content={siteDescription} />
+        <meta property="twitter:image" content="/og-preview.jpg" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
