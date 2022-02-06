06fcc6c2ea20e3bfdfa39f48bd5a8352c63f52ca
Alexander Chabin <nibach@ya.ru>
2022-02-06 13:44:20 +0500

    Починил баги в PageSpeed из-за Яндекс.Метрики

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -28,12 +28,6 @@ export default function Layout({ children, home }) {
       </div>
 
       <Footer />
-
-      <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{ __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");ym(57112834, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true });`, }}
-      />
-      <div dangerouslySetInnerHTML={{ __html: `<noscript><div><img src="https://mc.yandex.ru/watch/57112834" style="position:absolute; left:-9999px;" alt="" /></div></noscript>` }}></div>
     </>
   )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -30,6 +30,11 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `<script>(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");ym(57112834, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true });</script><noscript><div><img src="https://mc.yandex.ru/watch/57112834" style="position:absolute; left:-9999px;" alt="" /></div></noscript>` }}
+        />
       </body>
     </Html>
   )
