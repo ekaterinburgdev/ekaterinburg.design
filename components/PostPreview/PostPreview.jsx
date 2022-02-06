946f64ec37f22dfb3417cbc202226a1ee363072b
Alexander Chabin <nibach@ya.ru>
2022-02-06 19:39:03 +0500

    Добавил noopener noreferrer для target="_blank"

--- components/PostPreview/PostPreview.jsx
+++ components/PostPreview/PostPreview.jsx
@@ -7,10 +7,10 @@ import styles from './PostPreview.module.scss'
 const cx = classNames.bind(styles);
 
 export default function PostPreview({ title, siteLink, gallery, big, contrast }) {
-  const galleryRef = useRef(null)
+  const galleryRef = useRef(null);
 
   useEffect(() => {
-    const images = galleryRef.current.querySelectorAll('img')
+    const images = galleryRef.current.querySelectorAll('img');
 
     if (images.length > 1) {
       let startRandomTimeout;
@@ -22,18 +22,20 @@ export default function PostPreview({ title, siteLink, gallery, big, contrast })
           for (let i = 0; i < images.length; i++) {
             images[i].classList.remove(cx('post-preview__image_active'));
           }
-  
+
           current = (current != images.length - 1) ? current + 1 : 0;
           images[current].classList.add(cx('post-preview__image_active'));
         }, 4000);
       }, Math.floor(Math.random() * 1500) + 1)
-  
+
       return () => {
         clearTimeout(startRandomTimeout)
         clearInterval(changeSlidesInterval)
       };
     }
   }, [])
+
+  const isNewPageLink = siteLink.charAt(0) !== '/';
 
   return (
     <article className={cx(
@@ -53,7 +55,15 @@ export default function PostPreview({ title, siteLink, gallery, big, contrast })
         )}
       </span>
 
-      {siteLink && <a href={siteLink} target={siteLink.charAt(0) !== '/' ? '_blank' : '_self'}  className={cx('post-preview__link-area')}>{title}</a>}
+      {siteLink &&
+        <a
+          href={siteLink}
+          target={isNewPageLink ? '_blank' : null}
+          rel={isNewPageLink ? 'noopener noreferrer' : null}
+          className={cx('post-preview__link-area')}
+        >
+          {title}
+        </a>}
     </article>
   )
 };
