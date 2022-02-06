c36778d240d2be484bf31e0bdc4ecefec3244d59
Alexander Chabin <nibach@ya.ru>
2022-02-06 16:05:34 +0500

    Добавил DNS prefetch для Яндекс.Метрики

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,7 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <link rel="dns-prefetch" href="https://mc.yandex.ru/" />
       </Head>
       <body>
         <Main />
