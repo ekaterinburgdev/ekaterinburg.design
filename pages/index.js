d519f6703e605a92b7f1dead92369169886ccc0b
Anton Akhatov <anton@gcor.ru>
2023-02-08 00:05:27 +0000

    feature: add projects panel to footer

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,26 @@
-import { getNotionDatabaseItems } from '../core/notion'
+import { getNotionDatabaseItems } from "../core/notion";
 
-import Head from 'next/head'
+import Head from "next/head";
 
-import menuItems from '../routes.json';
+import menuItems from "../routes.json";
 
-import Layout from '../components/Layout'
-import Menu from '../components/Menu'
-import PostPreviewGrid from '../components/PostPreviewGrid'
-import Map from '../components/Map';
-import TeamList from '../components/TeamGrid';
-import Contacts from '../components/Contacts';
-import Partners from '../components/Partners/Partners';
-import Cover from '../components/Cover';
-import TextBlockComponent from '../components/TextBlock';
+import Layout from "../components/Layout";
+import Menu from "../components/Menu";
+import PostPreviewGrid from "../components/PostPreviewGrid";
+import Map from "../components/Map";
+import TeamList from "../components/TeamGrid";
+import Contacts from "../components/Contacts";
+import Partners from "../components/Partners/Partners";
+import Cover from "../components/Cover";
+import TextBlockComponent from "../components/TextBlock";
 
 export default function Home({ textBlocks, projects, team, partners }) {
-  const TextBlock = ({ name }) => <TextBlockComponent data={textBlocks} name={name} />
+  const TextBlock = ({ name }) => (
+    <TextBlockComponent data={textBlocks} name={name} />
+  );
 
   return (
-    <div className='wrap'>
+    <div className="wrap">
       <Cover />
       <Layout home>
         <Head>
@@ -27,23 +29,44 @@ export default function Home({ textBlocks, projects, team, partners }) {
 
         <Menu items={menuItems} />
 
-        <section id="about" className={'section'}>
-          <div className={'main-visual'}>
-            <h2 className={'main-visual__title'}>
+        <section id="about" className={"section"}>
+          <div className={"main-visual"}>
+            <h2 className={"main-visual__title"}>
               <TextBlock name="MainVisualTitle" />
             </h2>
-            <p className={'main-visual__description'}>
+            <p className={"main-visual__description"}>
               <TextBlock name="MainVisualText" />
 
-              <svg className={'main-visual__logo'} width="207" height="170" viewBox="0 0 207 170" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <svg
+                className={"main-visual__logo"}
+                width="207"
+                height="170"
+                viewBox="0 0 207 170"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <g clipPath="url(#clip0_1696_932)">
-                  <path d="M206.003 99.79C137.335 77.6443 68.6677 138.149 0 116.003V161.953C68.6677 184.099 137.335 123.594 206.003 145.74V99.79Z" fill="#00B4FF" />
-                  <path d="M206.003 7.95005C137.335 -14.1957 68.6677 46.3086 0 24.1628V70.0828C68.6677 92.2286 137.335 31.7243 206.003 53.8701V7.95005Z" fill="#FFD400" />
-                  <path d="M206.003 43.8701C137.335 21.7244 68.6677 82.2286 0 60.0829V126.003C68.6677 148.149 137.335 87.6444 206.003 109.79V43.8701Z" fill="#00B400" />
+                  <path
+                    d="M206.003 99.79C137.335 77.6443 68.6677 138.149 0 116.003V161.953C68.6677 184.099 137.335 123.594 206.003 145.74V99.79Z"
+                    fill="#00B4FF"
+                  />
+                  <path
+                    d="M206.003 7.95005C137.335 -14.1957 68.6677 46.3086 0 24.1628V70.0828C68.6677 92.2286 137.335 31.7243 206.003 53.8701V7.95005Z"
+                    fill="#FFD400"
+                  />
+                  <path
+                    d="M206.003 43.8701C137.335 21.7244 68.6677 82.2286 0 60.0829V126.003C68.6677 148.149 137.335 87.6444 206.003 109.79V43.8701Z"
+                    fill="#00B400"
+                  />
                 </g>
                 <defs>
                   <clipPath id="clip0_1696_932">
-                    <rect width="206.64" height="153.66" fill="white" transform="matrix(0.996917 -0.0784591 0 1 0 16.2129)" />
+                    <rect
+                      width="206.64"
+                      height="153.66"
+                      fill="white"
+                      transform="matrix(0.996917 -0.0784591 0 1 0 16.2129)"
+                    />
                   </clipPath>
                 </defs>
               </svg>
@@ -51,31 +74,39 @@ export default function Home({ textBlocks, projects, team, partners }) {
           </div>
         </section>
 
-        <section className={'section'} id="projects">
-          <h2 className={'section__heading section__heading_projects'}>Дизайн-<br />система</h2>
-          <p className={'section__subheader section__subheader_projects'}>
+        <section className={"section"} id="projects">
+          <h2 className={"section__heading section__heading_projects"}>
+            Дизайн-
+            <br />
+            система
+          </h2>
+          <p className={"section__subheader section__subheader_projects"}>
             <TextBlock data={textBlocks} name="MainProjectsText" />
           </p>
           <PostPreviewGrid posts={projects} />
         </section>
 
-        <section className={'section'} id="partners">
-          <h2 className={'section__heading section__heading_map'}>
+        <section className={"section"} id="partners">
+          <h2 className={"section__heading section__heading_map"}>
             <a href="https://map.ekaterinburg.design/" target="_blank">
-              Наши работы<br />в&nbsp;городе
+              Наши работы
+              <br />
+              в&nbsp;городе
             </a>
           </h2>
           <Map widgetUrl="https://map.ekaterinburg.design/widget" />
         </section>
 
-        <section className={'section'} id="partners">
-          <h2 className={'section__heading section__heading_partners'}>Партнёры</h2>
+        <section className={"section"} id="partners">
+          <h2 className={"section__heading section__heading_partners"}>
+            Партнёры
+          </h2>
 
           <Partners partners={partners} />
         </section>
 
-        <section className={'section'} id="team">
-          <h2 className={'section__heading section__heading_team'}>Команда</h2>
+        <section className={"section"} id="team">
+          <h2 className={"section__heading section__heading_team"}>Команда</h2>
 
           <TeamList team={team} />
         </section>
@@ -96,24 +127,27 @@ export default function Home({ textBlocks, projects, team, partners }) {
         </section>
         */}
 
-        <section className={'section'} id="contacts">
-          <h2 className={'section__heading section__heading_contacts'}>Почта<br />и соцсети</h2>
+        <section className={"section"} id="contacts">
+          <h2 className={"section__heading section__heading_contacts"}>
+            Почта
+            <br />и соцсети
+          </h2>
 
           <Contacts />
         </section>
       </Layout>
     </div>
-  )
+  );
 }
 
 export async function getStaticProps() {
   return {
     props: {
-      projects: await getNotionDatabaseItems('Projects'),
-      team: await getNotionDatabaseItems('Team'),
-      partners: await getNotionDatabaseItems('Partners'),
-      textBlocks: await getNotionDatabaseItems('TextBlocks')
+      projects: await getNotionDatabaseItems("Projects"),
+      team: await getNotionDatabaseItems("Team"),
+      partners: await getNotionDatabaseItems("Partners"),
+      textBlocks: [],
     },
     revalidate: 15,
-  }
+  };
 }
