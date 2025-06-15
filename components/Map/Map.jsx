0244ee41f5e269f260c3322a64d866ac4bfeee05
Alexander Chabin <alex.chabin@gmail.com>
2025-06-15 23:42:38 +0500

    Build files

--- components/Map/Map.jsx
+++
@@ -1,9 +0,0 @@
-import classNames from 'classnames/bind';
-
-import styles from './Map.module.scss';
-
-const cx = classNames.bind(styles);
-
-export default function Map({ widgetUrl }) {
-    return <iframe src={widgetUrl} className={cx(styles['map'])} lazy="true" />;
-}
--- components/TextBlock/TextBlock.jsx
+++
@@ -1,7 +0,0 @@
-export default function TextBlock({ data, name }) {
-  return (
-    <span dangerouslySetInnerHTML={{__html:
-      data.find(textBlock => textBlock.name === name)?.html || ''}}
-    />
-  )
-}
