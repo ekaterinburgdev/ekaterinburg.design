57d1dc1675ac0a0e232c5ebe83f7b6e82b0e99ba
Alexander Chabin <alex.chabin@gmail.com>
2026-01-13 01:54:48 +0500

    Show json with all team dev + design

--- components/TeamList/TeamList.jsx
+++ components/TeamList/TeamList.jsx
@@ -10,17 +10,29 @@ export default function TeamGrid({ team }) {
 
 
   return (
-    team && <ul className={cx("team-grid")}>
-      {team.map(({ name, photo, link, role }) => (
-        <li className={cx("team-grid__item")} key={name}>
-          <TeamPerson
-            name={name}
-            role={role}
-            photo={photo}
-            link={link}
-          />
-        </li>
-      ))}
-    </ul>
+    <>
+      <textarea style={{ width: '100%', height: 500 }}>
+        {JSON.stringify(team.map(x => {
+          return {
+            ...x,
+            link: x.link || null,
+            photo: x?.photo[0]?.replace('/notion-static/', 'https://ekaterinburg.design/notion-static/')
+          }
+        }), null, '  ')
+        .replaceAll('"undefined"', 'null')}
+      </textarea>
+      {team && <ul className={cx("team-grid")}>
+        {team.map(({ name, photo, link, role }) => (
+          <li className={cx("team-grid__item")} key={name}>
+            <TeamPerson
+              name={name}
+              role={role}
+              photo={photo}
+              link={link}
+            />
+          </li>
+        ))}
+      </ul>}
+    </>
   )
 };
